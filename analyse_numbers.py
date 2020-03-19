52ab71b39849ea32eb3ec1b58bfac70eff621540
Tobias Goldschmidt <220487@vut.cz>
2020-03-19 15:41:52 +0100

    Added new program

--- analyse_numbers.py
+++ analyse_numbers.py
@@ -1,11 +1,15 @@
 def is_number_natural(number):
     if number % 1 == 0 and number > 0:
         return True
-    else
+    else:
         return False
 
+
 def main():
-    my_number = 12
-    print(my_number, "is number natural", is_number_natural(my_number))
+    my_numbers = [12, 13, 14, 15, 16, 17, 134134, 311]
+    for i in my_numbers:
+        print(my_numbers[i - 1], "is number natural", is_number_natural(my_numbers[i - 1]))
+
+
 if __name__ == '__main__':
     main()
