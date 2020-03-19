dc8fc607fab517fa7f9e059e8dc44a19b4e30711
Tobias Goldschmidt <220487@vut.cz>
2020-03-19 14:34:05 +0100

    Add check for whole numbers

---
+++ analyse_numbers.py
@@ -0,0 +1,11 @@
+def is_whole_number(number):
+    if number % 1 == 0
+        return True
+    else
+        return False
+
+def main():
+    my_number = 12
+    print(my_number, "is whole nomber", is_whole_number(my_number))
+if __name__ == '__main__':
+    main()
