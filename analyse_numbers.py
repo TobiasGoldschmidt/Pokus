08d4cd59958fcf357d273f805a2bcd939a223320
Tobias Goldschmidt <220487@vut.cz>
2020-03-19 14:46:05 +0100

    add check if natural nummber

--- analyse_numbers.py
+++ analyse_numbers.py
@@ -1,11 +1,11 @@
-def is_whole_number(number):
-    if number % 1 == 0
+def is_number_natural(number):
+    if number % 1 == 0 and number > 0:
         return True
     else
         return False
 
 def main():
     my_number = 12
-    print(my_number, "is whole nomber", is_whole_number(my_number))
+    print(my_number, "is number natural", is_number_natural(my_number))
 if __name__ == '__main__':
     main()
